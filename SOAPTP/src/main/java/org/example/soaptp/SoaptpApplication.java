5ff28b11a4ad6856676b01ecd2d6c081129dc3bf
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-11-06 22:11:56 +0100

    Soap Folder created

---
+++ SOAPTP/src/main/java/org/example/soaptp/SoaptpApplication.java
@@ -0,0 +1,13 @@
+package org.example.soaptp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaptpApplication.class, args);
+    }
+
+}
---
+++ SOAPTP/src/test/java/org/example/soaptp/SoaptpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.soaptp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaptpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
